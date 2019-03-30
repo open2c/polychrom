b6a78478dd9f9172ddc0b90c48dc2d7440c2b13f
Maksim Imakaev <magus@wiz.mit.edu>
2019-03-30 17:41:03 -0400

    moved docks

--- openmmlib2/_openmmlib_doc/_build/_sources/knotAnalysis.txt
+++
@@ -1,8 +0,0 @@
-Knot complexity scripts
------------------------
-
-A set of small utilities to analyze knot complexity 
-
-.. automodule:: knotAnalysis 
-   :members: 
- 
--- openmmlib2/_openmmlib_doc/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sphinx-build . _build
-cp -r  _build/*  ../../manual_pages/mimakaev.bitbucket.org/
-cd ../../manual_pages/mimakaev.bitbucket.org
-hg add * 
-hg commit -m "Automatic commit from a new build" 
-hg push 
-
