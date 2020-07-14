d65c6381ef73ea077ed50ad9166a8fe22318a96c
kipolovnikov <36499892+kipolovnikov@users.noreply.github.com>
2020-07-14 17:57:11 +0300

    Update forces.py

--- polychrom/forces.py
+++ polychrom/forces.py
@@ -900,11 +900,11 @@ def grosberg_selective_repulsive_force(sim_object,
     force.addGlobalParameter('cut', sim_object.kT)
     force.addGlobalParameter('cut2', 0.5 * sim_object.kT)
     
-    #force.addPerParticleParameter("typee")
+    force.addPerParticleParameter("typee")
     force.addPerParticleParameter("trunc")
     
     for i in range(sim_object.N):  # adding all the particles on which force acts
-        force.addParticle((i, np.int(trunc_values[i])))
+        force.addParticle((i, [i, trunc_values[i]]))
         
     force.setCutoffDistance(nbCutOffDist)
     
