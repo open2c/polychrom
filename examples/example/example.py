1b4fbc2338c4a8bf65d92e3373d24bc1a4e8e600
Max Imakaev <mimakaev@gmail.com>
2022-03-13 11:58:49 -0400

    fixed import in example file as well

--- examples/example/example.py
+++ examples/example/example.py
@@ -9,7 +9,7 @@ In this simulation, a simple polymer chain of 10,000 monomers is
 import os, sys
 import polychrom
 from polychrom import simulation, starting_conformations, forces, forcekits
-import simtk.openmm as openmm
+import openmm
 import os
 from polychrom.hdf5_format import HDF5Reporter
 
